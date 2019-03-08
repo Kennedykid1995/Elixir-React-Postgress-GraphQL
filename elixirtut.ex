f66f829205abc532e8719278cf35a2de9f2648d0
Kennedykid1995 <34872885+Kennedykid1995@users.noreply.github.com>
2019-03-07 19:56:31 -0600

    continuing to learn elixir

--- elixirtut.ex
+++ elixirtut.ex
@@ -1,11 +1,23 @@
 defmodule M do
     def main do
-    # name = IO.gets("What is you name? ")|> String.trim
-    # IO.puts "Hello #{name}"
-    data_stuff()
+    do_stuff()
     end
-    def data_stuff do
-    my_int = 123
-    IO.puts "Integer #{is_integer(my_int)}"
+    def do_stuff do
+    age = 16
+    # IO.puts "Vote & Drive : #{(age >= 16) and (age >= 18)}" 
+    # IO.puts "Vote & Drive : #{(age >= 16) or (age >= 18)}" 
+
+    # IO.puts not true
+
+    if age >= 8 do
+        IO.puts "Can Vote"
+    else
+        IO.puts "Can't Vote"
+    end
+
+    unless age === 18 do
+        IO.puts "You are not 18"
+    else    
+        IO.puts "You are 18"
     end
 end
